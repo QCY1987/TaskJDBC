321015eefa9fcd48e0f0b46691eb2d73a54d168a
Антон <avdev1987@gmail.com>
2020-11-18 13:24:13 +0500

    Создаю подключение к БД

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,21 @@
 package jm.task.core.jdbc.util;
+import java.sql.*;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
-}
+    public Util() {
+        String url = "jdbc:mysql://localhost:3306/Mytest?serverTimezone=Europe/Moscow&useSSL=false";
+        String username = "root";
+        String password = "root";
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Молодец, соединение с БД успешно установлено");
+            } catch(Exception ex){
+                System.out.println("Ошибка, не удалось выполнить соединение с БД");
+            ex.printStackTrace();
+            }
+
+        }
+
+        }
+
+
